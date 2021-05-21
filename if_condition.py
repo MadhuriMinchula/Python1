1fdd6d3fa5b13b55917cd62e4f9a542bb3a220b3
Madhuri Minchula <84283321+MadhuriMinchula@users.noreply.github.com>
2021-05-21 21:08:58 +0530

    Add files via upload

---
+++ if_condition.py
@@ -0,0 +1,10 @@
+print("Hi")
+name = input("What is your name")
+print("It's nice to meet u,", name)
+
+answer = input("Are you enjoying the course? ")
+
+if answer == "Yes":
+    print("That's good to hear")
+
+print("Final Statement")
